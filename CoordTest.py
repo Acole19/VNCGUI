874d73750a79e7c5068d485f679a6dac405ca2b3
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2019-01-10 09:58:53 -0600

    Generated map

--- CoordTest.py
+++ CoordTest.py
@@ -1,1 +1,22 @@
-
+import Tkinter
+from Tkinter import *
+from PIL import Image
+root = Tk()
+canvas = Canvas(root, width=240, height = 242)
+canvas.create_oval(10, 10, 80, 80, outline="#f11",
+            width=2)
+z = [1,5,7,9]
+v = 0
+a = 1
+b = 1
+c = 2
+while v <= 3:
+    z.append(c)
+    a = b + c
+    b = a + c
+    c = a + b
+    v += 1
+    z.append(a)
+canvas.create_line(z)
+canvas.pack()
+root.mainloop()
