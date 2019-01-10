b09ba435bd6064e300b6f5882394e5bd4f87ef43
Grifferpentettah <33101789+Grifferpentettah@users.noreply.github.com>
2019-01-10 10:16:48 -0600

    established random int

--- Rand.py
+++ Rand.py
@@ -1,1 +1,17 @@
-
+import random
+from random import seed
+from random import randint
+# seed random number generator
+seed(1)
+global z
+z = 0
+x = []
+# generate some integers
+while z <= 100:
+    z += 1
+    if z <= 10:
+        y = randint(0,242)
+    else:
+        y = randint(0,242)
+        x.append(y)
+    print x
