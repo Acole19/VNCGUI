4f6a13a07cfdc4090a3b1e308d8300ad4cd53228
Acole19 <33101789+Acole19@users.noreply.github.com>
2019-02-15 10:38:11 -0600

    Update tick.py

--- ticking/tick.py
+++ ticking/tick.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 pygame.init()
 def play():
-    pygame.mixer.music.load(ti.mp3") #Loading File Into Mixer
+    pygame.mixer.music.load("ticking.mp3") #Loading File Into Mixer
     pygame.mixer.music.play(loops = 10) #Playing It In The Whole Device
 Button(root,text="Play",command=play).pack()
 root.mainloop()
